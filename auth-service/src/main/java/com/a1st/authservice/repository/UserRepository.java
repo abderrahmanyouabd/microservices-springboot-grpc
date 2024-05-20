a67b9f4733e954e62cf75ec9dbee63baa89f4f2a
abderrahmanyouabd <youabd50@gmail.com>
2024-05-20 18:04:31 +0200

    Added UserRepository with basic CRUD operations and email search functionality

---
+++ auth-service/src/main/java/com/a1st/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.a1st.authservice.repository;
+
+import com.a1st.authservice.model.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
+}
