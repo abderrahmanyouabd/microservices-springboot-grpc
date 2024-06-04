cbbad18b34c4ff8d18352c571cb9c54c70ed1641
abderrahmanyouabd <youabd50@gmail.com>
2024-06-05 01:43:18 +0200

    Added EmailAlreadyExistsException with basic structure and constructor

---
+++ patient-service/src/main/java/com/a1st/patientservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.a1st.patientservice.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+  public EmailAlreadyExistsException(String message) {
+    super(message);
+  }
+}
